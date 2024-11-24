41b42ac5a7d3e1a64fa4b7cc496bccd5be1e68a1
Arya Adinul Fadlan <adinul.fadlan04@gmail.com>
2024-11-24 12:08:48 +0700

    async vs then catch

--- app/page.tsx
+++ app/page.tsx
@@ -3,9 +3,23 @@ import Test from "@/components/test";
 import WithLoading from "@/hocs/with-loading";
 import db from "@/lib/db";
 
+function blockingAsyncOperation() {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(``);
+      console.log("DUA");
+    }, 2000);
+  });
+}
 const TestWithLoading = WithLoading(Test);
 export default async function Home() {
   const posts = await db.post.findMany({ orderBy: { createdAt: "desc" } });
+  console.log("SATU");
+  // await blockingAsyncOperation();
+  blockingAsyncOperation()
+    .then((res) => console.log(res))
+    .catch((err) => console.log(err));
+  console.log("TIGA");
   // const externalAPIs = await fetch("http://localhost:4000/api/users");
   // const response = await externalAPIs.json();
   // console.log({ externalAPIsResponse: response });
